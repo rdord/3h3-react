d2240431f0da9b1ac1e695ea1a3552acfce04bbb
Roland Đorđević <roland.djordjevic@ineor.si>
2019-12-26 15:00:23 +0100

    shop page, collection preview and collection item

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+function ShopPage() {
+  const [collections, setCollections] = useState(SHOP_DATA);
+
+  return (
+    <div className='shop-page'>
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+}
+
+export default ShopPage;
