ca2ea2e01f3f13607e51fbe3bc3bf0eaeb4a13df
Roland Đorđević <roland.djordjevic@ineor.si>
2019-12-26 19:01:38 +0100

    add auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribeFromAuth = () =>
+      auth.onAuthStateChanged(user => {
+        setCurrentUser(user);
+        console.log(user);
+      });
+
+    return unsubscribeFromAuth();
+  }, []);
+
   return (
     <div>
       <Header />
