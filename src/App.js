9dd087fc69e2b31d75068f1063f537a677ef829b
Roland Đorđević <roland.djordjevic@ineor.si>
2020-01-06 20:52:24 +0100

    sign in with email and password

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
     };
 
     return unsubscribeFromAuth();
-  }, [currentUser]);
+  }, []);
 
   return (
     <div>
--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -2,14 +2,21 @@ import React, { useState } from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = event => {
+  const handleSubmit = async event => {
     event.preventDefault();
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.error('sign-up in', error.message);
+    }
+
     setEmail('');
     setPassword('');
   };
